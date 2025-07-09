c17561aa911aa12c731f2d46d13473b63ea26dcf
Saafet <safet.srna@fpmoz.sum.ba>
2025-07-09 12:29:03 +0200

    milisekunde

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <omp.h>
+#include <cmath>
 
 int main() {
     std::cout << "Pokrecem obradu slike...\n";
@@ -13,21 +14,32 @@ int main() {
 
     cv::Mat grad(img.rows, img.cols, CV_8UC1);
 
+    const uchar* src = img.ptr<uchar>();
+    uchar* dst = grad.ptr<uchar>();
+    size_t step = img.step;
+
     double t0 = omp_get_wtime();
 
-    #pragma omp parallel for collapse(2)
+    #pragma omp parallel for
     for (int y = 1; y < img.rows - 1; y++) {
         for (int x = 1; x < img.cols - 1; x++) {
-            int gx = -img.at<uchar>(y-1, x-1) - 2*img.at<uchar>(y, x-1) - img.at<uchar>(y+1, x-1)
-                     + img.at<uchar>(y-1, x+1) + 2*img.at<uchar>(y, x+1) + img.at<uchar>(y+1, x+1);
-            int gy = -img.at<uchar>(y-1, x-1) - 2*img.at<uchar>(y-1, x) - img.at<uchar>(y-1, x+1)
-                     + img.at<uchar>(y+1, x-1) + 2*img.at<uchar>(y+1, x) + img.at<uchar>(y+1, x+1);
-            grad.at<uchar>(y, x) = std::min(255, (int)sqrt(gx*gx + gy*gy));
+            int idx = y * step + x;
+
+            int gx = -src[idx - step - 1] - 2 * src[idx - 1] - src[idx + step - 1]
+                     + src[idx - step + 1] + 2 * src[idx + 1] + src[idx + step + 1];
+
+            int gy = -src[idx - step - 1] - 2 * src[idx - step] - src[idx - step + 1]
+                     + src[idx + step - 1] + 2 * src[idx + step] + src[idx + step + 1];
+
+            
+            int val = abs(gx) + abs(gy);
+            dst[idx] = (uchar)(val > 255 ? 255 : val);
         }
     }
 
     double t1 = omp_get_wtime();
-    std::cout << "Vrijeme CPU obrade (OpenMP): " << (t1 - t0) << " sekundi\n";
+
+    std::cout << "Vrijeme CPU obrade: " << (t1 - t0) * 1000 << " ms\n";
 
     cv::imwrite("output_cpu.jpg", grad);
 
